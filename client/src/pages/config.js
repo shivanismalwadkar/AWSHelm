c69eb4940e701aff780f122db1a2202b763e8e67
shivanismalwadkar <shivanimalwadkar@gmail.com>
2025-04-04 17:10:04 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://veera.narni.co.in";
+const API_BASE_URL = "http://aws-helm-shivani.com/backend";
 export default API_BASE_URL;
